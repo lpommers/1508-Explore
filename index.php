ba86e1dd909e57a711cac786d8bf3bf31dde1638
ninalouise <ninaolafsson@Ninas-MacBook-Pro.local>
2013-12-11 15:22:50 +0100

    made divs for the content

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
 <head>
 	<meta charset="UTF-8">
 	<title>1508 explore</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
 </head>
 <body>
-
+	<div id="banner"> 
+		Banner/Logo
+	</div>
+	<div id="menu">
+		Navigation menu
+	</div>
+	<div id="photo">
+		photo/video
+	</div>
+	<div id="photo">
+		photo/video
+	</div>
+	<div id="photo">
+		photo/video
+	</div>
 </body>
 </html>
