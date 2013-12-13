4b5f367291e034e7d40b59428a7a0e6c460c6450
Luke Pommersheim <lpommers@gmail.com>
2013-12-13 11:46:21 +0100

    homepage / css

--- index.php
+++ index.php
@@ -10,12 +10,63 @@
 </head>
 <body>
 	<header>
-		<a href="#"><img src="img/logo.png" alt="1508 logo"></a>
-	<nav>
-		<a href="#">About us</a>
-		<a href="http://1508.dk">1508.dk</a>
+		<a href="#"><img id='logo'src="img/logo.png" alt="1508 logo"></a>
+
+		<nav class='header_nav'>
+			<div class='header_about_links'>
+				<a id = 'about_us_link' href="#">About us</a>
+				<a href="http://1508.dk">1508.dk</a>
+			</div>
+
+			<div class='social_icons'>
+				<a href="#"><img src="img/linkedin.png" alt="linkedin"></a>
+				<a href="#"><img src="img/facebook.png" alt="facebook"></a>
+				<a href="#"><img src="img/twitter.png" alt="twitter"></a>
+				<a href="#"><img src="img/vimeo.png" alt="vimeo"></a>
+			</div>
+		</nav>
+	</header>
+
+	<nav class="filter_nav">
+		<a href="#">all</a>
+		<a href="#">events</a>
+		<a href="#">knowledge</a>
+		<a href="#"><img src="img/search.png" alt="search"></a>
 	</nav>
 
-	</header>
+	<div>
+		<h2>Lorum Ipsum</h2>
+		<h3>12 DEC 2013</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula rhoncus lorem, in auctor dui accumsan eget. Duis euismod tempor pellentesque. Nulla erat leo, varius eget felis non, volutpat ultricies eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras congue auctor erat, eget hendrerit augue convallis a.
+		</p>
+
+	</div>
+
+	<div>
+		<h2>Lorum Ipsum</h2>
+		<h3>12 DEC 2013</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula rhoncus lorem, in auctor dui accumsan eget. Duis euismod tempor pellentesque. Nulla erat leo, varius eget felis non, volutpat ultricies eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras congue auctor erat, eget hendrerit augue convallis a.
+		</p>
+
+	</div>
+
+	<div>
+		<h2>Lorum Ipsum</h2>
+		<h3>12 DEC 2013</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula rhoncus lorem, in auctor dui accumsan eget. Duis euismod tempor pellentesque. Nulla erat leo, varius eget felis non, volutpat ultricies eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras congue auctor erat, eget hendrerit augue convallis a.
+		</p>
+	</div>
+
+	<div>
+		<h2>Lorum Ipsum</h2>
+		<h3>12 DEC 2013</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula rhoncus lorem, in auctor dui accumsan eget. Duis euismod tempor pellentesque. Nulla erat leo, varius eget felis non, volutpat ultricies eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras congue auctor erat, eget hendrerit augue convallis a.
+		</p>
+
+	</div>
+
+
+
+
 </body>
 </html>
