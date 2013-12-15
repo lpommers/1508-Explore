1c4ee540bc6d1ffe20d0b942cf8d5b82d1f3c489
Luke Pommersheim <lpommers@gmail.com>
2013-12-15 12:10:35 +0100

    stylesheet

--- index.php
+++ index.php
@@ -60,52 +60,55 @@
 </head>
 <body>
 		<header>
-		<a href="#"><img id='logo'src="img/logo.png" alt="1508 logo"></a>
-			<div class='header_about_links'>
-				<div><a href="#">about</a></div>
-				<div><a href="http://1508.dk">1508.dk</a></div>
-			</div>
+			<a href="#"><img id='logo'src="img/logo.png" alt="1508 logo"></a>
+				<div class='header_about_links'>
+					<div><a href="#">about</a></div>
+					<div><a href="http://1508.dk">1508.dk</a></div>
+				</div>
 
-			<ul class='social_icons'>
+				<ul class='social_icons'>
+					<li>
+						<a href="http://www.linkedin.com/company/22451?trk=tyah&trkInfo=tas%3A1508%2Cidx%3A1-1-1" target='_blank'><img src="img/social/1508linked.png" alt="linkedin"></a>
+					</li>
+					<li>
+						<a href="https://www.facebook.com/1508dk" target='_blank'><img src="img/social/1508fb.png" alt="facebook"></a>
+					</li>
+					<li>
+						<a href="http://www.slideshare.net/1508as" target='_blank'><img src="img/social/1508slide.png" alt="twitter"></a>
+					</li>
+					<li>
+						<a href="https://twitter.com/1508AS" target='_blank'><img src="img/social/1508tw.png" alt="vimeo"></a>
+					</li>
+					<li>
+						<a href="http://vimeo.com/design1508/videos" target='_blank'><img src="img/social/1508vim.png" alt="vimeo"></a>
+					</li>
+				</ul>
+			</header>
+
+		<div class="filter_wrapper">
+			<form id = 'search_box'>
+				<input type="text" value = 'Search'>
+			</form>
+			<ul class="filter_nav">
 				<li>
-					<a href="http://www.linkedin.com/company/22451?trk=tyah&trkInfo=tas%3A1508%2Cidx%3A1-1-1" target='_blank'><img src="img/social/1508linked.png" alt="linkedin"></a>
+					<a href="#"><img src="img/nav-icons/house.png" alt="home"></a>
 				</li>
 				<li>
-					<a href="https://www.facebook.com/1508dk" target='_blank'><img src="img/social/1508fb.png" alt="facebook"></a>
+					<a href="http://luke0086.keaweb.dk/1508?page=events"><img src="img/nav-icons/cal.png" alt="cal"></a>
 				</li>
 				<li>
-					<a href="http://www.slideshare.net/1508as" target='_blank'><img src="img/social/1508slide.png" alt="twitter"></a>
+					<a href="http://luke0086.keaweb.dk/1508?page=knowledge"><img src="img/nav-icons/light.png" alt="knowledge"></a>
 				</li>
 				<li>
-					<a href="https://twitter.com/1508AS" target='_blank'><img src="img/social/1508tw.png" alt="vimeo"></a>
+					<a href="http://luke0086.keaweb.dk/1508?page=people"><img src="img/nav-icons/people.png" alt="speakers"></a>
 				</li>
 				<li>
-					<a href="http://vimeo.com/design1508/videos" target='_blank'><img src="img/social/1508vim.png" alt="vimeo"></a>
+					<a href="#"><img src="img/nav-icons/search.png" alt="search"></a>
 				</li>
 			</ul>
-		</header>
-
-	<div class="filter_wrapper">
-		<ul class="filter_nav">
-			<li>
-				<a href="#"><img src="img/nav-icons/house.png" alt="home"></a>
-			</li>
-			<li>
-				<a href="http://luke0086.keaweb.dk/1508?page=events"><img src="img/nav-icons/cal.png" alt="cal"></a>
-			</li>
-			<li>
-				<a href="http://luke0086.keaweb.dk/1508?page=knowledge"><img src="img/nav-icons/light.png" alt="knowledge"></a>
-			</li>
-			<li>
-				<a href="http://luke0086.keaweb.dk/1508?page=people"><img src="img/nav-icons/people.png" alt="speakers"></a>
-			</li>
-			<li>
-				<a href="#"><img src="img/nav-icons/search.png" alt="search"></a>
-			</li>
-		</ul>
-	</div>
-	<div class='content'>
-		<?php echo $out; ?>
-	</div>
-</body>
-</html>
+		</div>
+		<div class='content'>
+			<?php echo $out; ?>
+		</div>
+	</body>
+	</html>
